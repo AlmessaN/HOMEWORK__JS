e3acddf136a0681693fd2d9ceb4f0426fc8586b3
Stepan Almes <shtukatulk@gmail.com>
2023-02-11 22:04:28 +0200

    task0

--- HW_2/homework2.js/task_0.js
+++ HW_2/homework2.js/task_0.js
@@ -2,4 +2,12 @@ let a = parseFloat(prompt('a=', '0'))
 let b = parseFloat(prompt('b=', '0'))
 
 let s1 = a+12+b
-document.write(`<h1>S1= ${s1}</h1>`)
+document.write(`<h1>S1 = ${s1}</h1>`)
+
+let secFirst = a+b
+let secSecond = 2*a
+let secTh = secFirst/secSecond
+
+let s2 = Math.sqrt(secTh)
+document.write(`<h1>S2 = ${s2.toFixed(2)}</h1>`)
+
