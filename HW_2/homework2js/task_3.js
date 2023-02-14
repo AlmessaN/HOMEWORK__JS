24cf2987037343548123b3a807da778d2c86ec6c
Stepan Almes <shtukatulk@gmail.com>
2023-02-14 23:24:59 +0200

    new9

--- HW_2/homework2js/task_3.js
+++ HW_2/homework2js/task_3.js
@@ -3,7 +3,7 @@ let b = parseFloat(prompt('Введіть ціну товару', '0'));
 
 let sum = a*b;
 
-let pdv = sum*0.005;
+let pdv = sum*0.05;
 
 document.write(`<h1>Загальна вартість = ${sum}</h1>`);
-document.write(`<h1>Вартість ПДФ = ${pdv.toFixed(2)}</h1>`);
+document.write(`<h1>Вартість ПДВ = ${pdv.toFixed(2)}</h1>`);
